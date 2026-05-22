8cf7d5b181f609c90e9f09af8d80141984ed1544
ST10540680 <ST10540680@myemeris.edu.za>
2026-05-22 11:33:48 +0200

    declared arrays and added a counter

--- app/src/main/java/com/example/numberstorage/MainActivity.kt
+++ app/src/main/java/com/example/numberstorage/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.numberstorage
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Declaration of the array
+    val number = arrayOf(10)
+
+    //counter to track which element to store the next
+    val counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         //Declaring variables
         val edtNum = findViewById<EditText>(R.id.edtNum)
         val edtAdd = findViewById<Button>(R.id.edtAdd)
+        val edtAvg = findViewById<Button>(R.id.edtAvg)
+        val txtAvg = findViewById<TextView>(R.id.txtAvg)
 
         //
 
